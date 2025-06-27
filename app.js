39c0a689398d974b9b21619cfb89c9d7c3a17f29
Suman Regmi <limitlessplaying@gmail.com>
2025-06-27 15:11:04 +0545

    Nodemon implementation

--- app.js
+++ app.js
@@ -8,11 +8,22 @@ const app = require('express')()
 app.get('/', (req, res) => {
     res.send('Hello World!') // send a response back to the client
 })
-app.get('/test', (req, res) => { // the client will make a GET request to this route
-    res.send('This is test page')
+app.get('/test', (req, res) => {
+    res.send('Hello Test!') // send a response back to the client
+})
+app.get('/profile', (req, res) => { // the client will make a GET request to this route
+    res.json({
+        'name': 'John Doe',
+        'age': 30,
+        'email': 'johndoe@example.com',
+        'address': "China town",
+    }) // send a JSON response back to the client
 })
 
 //for listening the server
 app.listen(3000, (req, res) => { // the server will run in port 3000
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port 3000...')
+    console.log('Click the link given below to open in browser')
+    console.log('http://127.0.0.1:3000')
+    console.log('Press Ctrl+C to exit!')
 })
